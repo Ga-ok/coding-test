9205598bb851e0b1e0d7f833996d3d16a86d8860
gaok <dlrkdhr321@gmail.com>
2022-05-27 00:57:09 +0900

    [SWEA] 1976 시각덧셈 / D2 / 6분

---
+++ [swea]1976.py
@@ -0,0 +1,21 @@
+# 시각 덧셈
+
+t=int(input())
+for testcase in range(1, t+1):
+
+    h1, m1, h2, m2 = map(int, input().split())
+
+    h_res=h1+h2
+
+    if h_res>12:
+        h_res=h_res-12
+
+    m_res=0
+    if m1+m2>=60:
+        h_res+=1
+        m_res=m1+m2 - 60
+
+    else:
+        m_res=m1+m2
+
+    print('#'+str(testcase), h_res, m_res)
